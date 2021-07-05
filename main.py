5d82e74797b98987b4367c0beaf97aa2b278480d
Jonathan Morales <jony.hackmc@gmail.com>
2021-07-05 14:09:51 -0500

    exponer servicio

--- main.py
+++ main.py
@@ -8,5 +8,5 @@ api = Api(app)
 # worker python main.py
 api.add_resource(HolaMundo, '/')
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+# if __name__ == '__main__':
+#     app.run(debug=True, host='0.0.0.0')
